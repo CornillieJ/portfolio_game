6a30917d3bc3a1aba38e4781af6cef80053f07ef
Jeffrey Cornillie <jeffrey.cornillie@student.howest.be>
2024-05-11 17:12:29 +0200

    Normalize vector on diagonal movement

--- Portfolio_Game_Core/Entities/Player.cs
+++ Portfolio_Game_Core/Entities/Player.cs
@@ -4,7 +4,7 @@ using Portfolio_Game_Core.Interfaces;
 
 namespace Portfolio_Game_Core;
 
-public class Player: GameObject, IMovable, IVisible
+public class Player : GameObject, IMovable, IVisible
 {
     public static Texture2D Texture { get; set; }
     public float Speed { get; set; } = 150;
@@ -22,46 +22,51 @@ public class Player: GameObject, IMovable, IVisible
         CurrentSprite = SpriteData.DownSprites[0];
         _spriteNumber = 0;
     }
-    public Player(int x, int y): this()
+
+    public Player(int x, int y) : this()
     {
         PositionX = x;
         PositionY = y;
     }
-    public Player(Texture2D texture, int x, int y): this(x,y)
+
+    public Player(Texture2D texture, int x, int y) : this(x, y)
     {
         Texture = texture;
     }
+
     public Texture2D GetStaticTexture()
     {
         return Texture;
     }
+
     public void SetStaticTexture(Texture2D texture)
     {
         Texture = texture;
     }
+
     public void GoRight(float deltaTime, bool canMove)
     {
-        if(canMove)
-            PositionX += (int) (Speed * deltaTime);
+        if (canMove)
+            PositionX += (Speed * deltaTime);
         if (Direction == Direction.Right)
             _spriteNumber++;
         else
             _spriteNumber = 0;
         if (_spriteNumber == 4 * _spriteChangeSpeed) _spriteNumber = 0;
-        CurrentSprite = SpriteData.RightSprites[_spriteNumber/_spriteChangeSpeed];
+        CurrentSprite = SpriteData.RightSprites[_spriteNumber / _spriteChangeSpeed];
         Direction = Direction.Right;
     }
 
     public void GoLeft(float deltaTime, bool canMove)
     {
-        if(canMove)
-            PositionX -= (int) (Speed * deltaTime);
+        if (canMove)
+            PositionX -= (Speed * deltaTime);
         if (Direction == Direction.Left)
             _spriteNumber++;
         else
             _spriteNumber = 0;
         if (_spriteNumber == 4 * _spriteChangeSpeed) _spriteNumber = 0;
-        CurrentSprite = SpriteData.LeftSprites[_spriteNumber/_spriteChangeSpeed];
+        CurrentSprite = SpriteData.LeftSprites[_spriteNumber / _spriteChangeSpeed];
         Direction = Direction.Left;
     }
 
@@ -69,32 +74,58 @@ public class Player: GameObject, IMovable, IVisible
     {
         if (canMove)
         {
-            PositionY -= (int) (Speed * deltaTime);
-            if(secondDirection == Direction.Left) PositionX -= (int) (Speed * deltaTime); 
-            if(secondDirection == Direction.Right) PositionX += (int) (Speed * deltaTime);
+            switch (secondDirection)
+            {
+                case Direction.Left:
+                    PositionX -= (float)(Speed * 0.71 * deltaTime);
+                    PositionY -= (float)(Speed * 0.71 * deltaTime);
+                    break;
+                case Direction.Right:
+                    PositionX += (float)(Speed * 0.71 * deltaTime);
+                    PositionY -= (float)(Speed * 0.71 * deltaTime);
+                    break;
+                default:
+                    PositionY -= (Speed * deltaTime);
+                    break;
+            }
         }
+
         if (Direction == Direction.Up)
             _spriteNumber++;
         else
             _spriteNumber = 0;
-        if (_spriteNumber == 4 * _spriteChangeSpeed) _spriteNumber = 0;
-        CurrentSprite = SpriteData.UpSprites[_spriteNumber/_spriteChangeSpeed];
+        if (_spriteNumber == 4 * _spriteChangeSpeed)
+            _spriteNumber = 0;
+        CurrentSprite = SpriteData.UpSprites[_spriteNumber / _spriteChangeSpeed];
         Direction = Direction.Up;
     }
+
     public void GoDown(float deltaTime, bool canMove, Direction secondDirection = Direction.Neutral)
     {
         if (canMove)
         {
-            PositionY += (int) (Speed * deltaTime);
-            if(secondDirection == Direction.Left) PositionX -= (int) (Speed * deltaTime); 
-            if(secondDirection == Direction.Right) PositionX += (int) (Speed * deltaTime);
+            switch (secondDirection)
+            {
+                case Direction.Left:
+                    PositionX -= (float)(Speed * 0.71 * deltaTime);
+                    PositionY += (float)(Speed * 0.71 * deltaTime);
+                    break;
+                case Direction.Right:
+                    PositionX += (float)(Speed * 0.71 * deltaTime);
+                    PositionY += (float)(Speed * 0.71 * deltaTime);
+                    break;
+                default:
+                    PositionY += (Speed * deltaTime);
+                    break;
+            }
         }
+
         if (Direction == Direction.Down)
             _spriteNumber++;
         else
             _spriteNumber = 0;
         if (_spriteNumber == 4 * _spriteChangeSpeed) _spriteNumber = 0;
-        CurrentSprite = SpriteData.DownSprites[_spriteNumber/_spriteChangeSpeed];
+        CurrentSprite = SpriteData.DownSprites[_spriteNumber / _spriteChangeSpeed];
         Direction = Direction.Down;
     }
 
@@ -102,18 +133,16 @@ public class Player: GameObject, IMovable, IVisible
     {
         switch (Direction)
         {
-           case Direction.Down:
-               return new Vector2(PositionX + Width/2, Middle.Y + distance);
-           case Direction.Up:
-               return new Vector2(PositionX + Width/2, Middle.Y - distance);
-           case Direction.Right:
-               return new Vector2(Right + distance, PositionY + Height/2);
-           case Direction.Left:
-               return new Vector2(Left - distance, PositionY + Height/2);
-           default:
-               return new Vector2(0, 0);
+            case Direction.Down:
+                return new Vector2(PositionX + Width / 2, Middle.Y + distance);
+            case Direction.Up:
+                return new Vector2(PositionX + Width / 2, Middle.Y - distance);
+            case Direction.Right:
+                return new Vector2(Right + distance, PositionY + Height / 2);
+            case Direction.Left:
+                return new Vector2(Left - distance, PositionY + Height / 2);
+            default:
+                return new Vector2(0, 0);
         }
     }
-
-
 }
